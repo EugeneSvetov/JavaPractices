e79eb3c38d2c775e62d049863278f43e5b701a57
o_0 <90154302+EugeneSvetov@users.noreply.github.com>
2023-10-07 16:47:23 +0300

    Create Bill.java

---
+++ hotel/src/Bill.java
@@ -0,0 +1,15 @@
+public class Bill {
+    private Integer amount;
+
+    public Bill(Integer amount) {
+        this.amount = amount;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+}
